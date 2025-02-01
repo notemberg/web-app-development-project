ad974745cf345d730dd9163b9813ba23b8ce6e9a
Sorrasak Limthong <aot123321ann@gmail.com>
2025-02-02 02:43:56 +0700

    -add tag

--- MaJerGan/Controllers/TagController.cs
+++ MaJerGan/Controllers/TagController.cs
@@ -1,7 +1,6 @@
 using MaJerGan.Data;
 using MaJerGan.Models;
 using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace MaJerGan.Controllers
@@ -17,11 +16,32 @@ namespace MaJerGan.Controllers
             _context = context;
         }
 
+        // ✅ ดึงแท็กทั้งหมดจากฐานข้อมูล
         [HttpGet]
         public IActionResult GetTags()
         {
             var tags = _context.Tags.Select(t => t.Name).ToList();
             return Ok(tags);
+        }
+
+        // ✅ เพิ่มแท็กใหม่เข้า Database
+        [HttpPost]
+        public IActionResult AddTag([FromBody] Tag newTag)
+        {
+            if (string.IsNullOrWhiteSpace(newTag.Name))
+            {
+                return BadRequest("Tag name cannot be empty");
+            }
+
+            // ตรวจสอบว่ามีแท็กนี้อยู่แล้วหรือไม่
+            if (_context.Tags.Any(t => t.Name == newTag.Name))
+            {
+                return Conflict("This tag already exists");
+            }
+
+            _context.Tags.Add(newTag);
+            _context.SaveChanges();
+            return Ok(newTag.Name);
         }
     }
 }
--- MaJerGan/obj/Debug/net9.0/MaJerGan.AssemblyInfo.cs
+++ MaJerGan/obj/Debug/net9.0/MaJerGan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaJerGan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b17a9f3de9c638552270a446533885007cf5e5b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+540ce84461d700a412aa911a5af4552a81cb5d41")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaJerGan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaJerGan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
