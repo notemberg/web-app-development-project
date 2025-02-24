f7fc3b5a0c51ccc0b6f7810942ece436e6eef692
Sorrasak Limthong <aot123321ann@gmail.com>
2025-02-24 21:41:56 +0700

    Updated appsettings and project files

--- MaJerGan/obj/Debug/net9.0/MaJerGan.AssemblyInfo.cs
+++ MaJerGan/obj/Debug/net9.0/MaJerGan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaJerGan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+709c0ca4e76f7d9b338047e029579dc24fde41fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00c7c7cdb49d912ae31d58299e24c80f79f5c692")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaJerGan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaJerGan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
