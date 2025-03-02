a8ad60fab663d0dd9536357ddd448cc9be48e8af
Sorrasak Limthong <aot123321ann@gmail.com>
2025-03-02 19:37:05 +0700

    Merge From Pingping manual

--- MaJerGan/obj/Debug/net9.0/MaJerGan.AssemblyInfo.cs
+++ MaJerGan/obj/Debug/net9.0/MaJerGan.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MaJerGan")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef86610bb54f1583c05701644227b14c513d778")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b5179e19ac27ad0669ce4099d1607372b98cdc0")]
 [assembly: System.Reflection.AssemblyProductAttribute("MaJerGan")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MaJerGan")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
